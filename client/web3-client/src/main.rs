da417244f1bd54531d853d899329870ccd8ec1b7
maksloboda <>
2023-02-28 20:00:14 +0000

    Wont work

--- client/web3-client/src/main.rs
+++ client/web3-client/src/main.rs
@@ -1,13 +1,18 @@
 use std::env;
-use ethers::providers::{Provider, Http, Middleware};
+use ethers::providers::{Provider, Http};
+use ethers::prelude::SignerMiddleware;
 use std::convert::TryFrom;
 use serde_json;
 use serde::{Serialize, Deserialize};
 use ethers::core::{abi::Abi, types::Address, types::U256, types::H256, types::Filter};
 use ethers::contract::{Contract, EthEvent};
-use ethers::signers::Wallet;
-use ethers::core::abi::{Detokenize, Token, InvalidOutputType};
+use ethers::signers::LocalWallet;
+use ethers::signers::Signer;
+use ethers::core::abi::{Detokenize, Token, InvalidOutputType, EventExt};
 use std::fs;
+use eth_keystore::encrypt_key;
+use rand::RngCore;
+use std::path::Path;
 
 #[derive(Clone, Debug, EthEvent)]
 struct Transfer {
@@ -17,6 +22,16 @@ struct Transfer {
     to: Address,
     #[ethevent(name = "_tokens")]
     tokens: U256,
+}
+
+#[derive(Clone, Debug, EthEvent)]
+struct Approval {
+    #[ethevent(indexed, name = "_owner")]
+    owner: Address,
+    #[ethevent(indexed, name = "_spender")]
+    spender: Address,
+    #[ethevent(name = "_value")]
+    value: U256,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -30,35 +45,61 @@ async fn main() -> Result<(), ()> {
     let provider = Provider::<Http>::try_from(
         &env::var("INFURA_HTTPS").unwrap()
     ).unwrap();
-
     let &address = &env::var("CONTRACT_ADDRESS").unwrap().parse::<Address>().unwrap();
-
+    // Load compilation artifact and construct abi from it
     let contract_info = serde_json::from_str::<ContractInfo>(&fs::read_to_string("../../artifacts/contracts/AmogusToken.sol/AmogusToken.json").unwrap()).unwrap();
-
     let abi: Abi = contract_info.abi;
+    let wallet: LocalWallet = env::var("GOERLI_PRIVATE_KEY").unwrap().parse().unwrap();
+    let client = SignerMiddleware::new(provider, wallet);
+    let contract = Contract::new(address, abi, client);
 
-    let contract = Contract::new(address, abi, provider);
-
+    // Call read only method
     let init_value: U256 = contract
         .method::<_, U256>("totalSupply", ()).unwrap()
         .call()
         .await.unwrap();
-
     println!("{}", init_value);
 
+    // Request transfer events
     let logs: Vec<Transfer> = contract
         .event_for_name("Transfer").unwrap()
         .from_block(0u64)
         .query()
         .await.unwrap();
-
     println!("{:?}", logs);
 
-    // let block = provider.get_block(100u64).await.unwrap();
-    // println!("Got block: {}", serde_json::to_string(&block).unwrap());
-    
-    // let addr = "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359".parse::<Address>().unwrap();
-    // let code = provider.get_code(addr, None).await.unwrap();
-    // println!("Got code: {}", serde_json::to_string(&code).unwrap());
+
+    // Request transfer events but only for certain receiving address
+    let other_addr = "0xab854be0a4d499b6fd8d0bb5f796ab5b33ce825b".parse::<Address>().unwrap();
+    let event = &contract.abi().event("Transfer").unwrap();
+    let logs: Vec<Transfer> = contract
+        .event_with_filter(Filter::new().event(&event.abi_signature()).topic2(H256::from(other_addr)))
+        .from_block(0u64)
+        .query()
+        .await.unwrap();
+    println!("{:?}", logs);
+
+    // Check approvals before call    
+    let logs: Vec<Transfer> = contract
+        .event_for_name("Approval").unwrap()
+        .from_block(0u64)
+        .query()
+        .await.unwrap();
+    println!("{:?}", logs);
+
+    // Now lets try calling the some methods and checking the events
+    let call = contract.method::<_, H256>("approve", (other_addr, U256::from(20))).unwrap();
+    let pending_tx = call.send().await.unwrap();
+    let receipt = pending_tx.confirmations(6).await.unwrap();
+    println!("{:?}", receipt);
+
+    // Check aprovals after call
+    let logs: Vec<Transfer> = contract
+        .event_for_name("Approval").unwrap()
+        .from_block(0u64)
+        .query()
+        .await.unwrap();
+    println!("{:?}", logs);
+
     Ok(())
 }
